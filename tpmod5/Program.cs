c1bc189dad3d52826e4070d5ab8a7c009ad3170a
unknown <hsilassamban@gmail.com>
2024-03-18 04:02:18 +0700

    first commit

---
+++ tpmod5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmod5
+{
+    internal class Program
+    {
+        public class Datageneric<T>
+        {
+            private T data;
+
+            public Datageneric(T data)
+            {
+                this.data = data;
+            }
+
+            public void printInfo()
+            {
+                Console.WriteLine("Data yang tersimpan =  " + this.data);
+            }
+        }
+
+        public class HaloGeneric
+        {
+            public static void SapaUser<T>(T data)
+            {
+                Console.WriteLine("Halo User " + data);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            HaloGeneric.SapaUser<string>("Henri");
+            Datageneric<String> test = new Datageneric<String>("1302220087");
+            test.printInfo();
+        }
+            
+        }
+    }
+
